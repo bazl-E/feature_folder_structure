255fda8296a77a6a2bedb8f6872a03576924647e
bazl-E <basil8592@gmail.com>
2024-01-05 14:35:17 +0530

    fix(cubit):brick name issue fix

--- bricks/cubit/__brick__/{{name.lowerCase()}}/logic/{{name.lowerCase()}}_cubit/{{name.lowerCase()}}_cubit.dart
+++ bricks/cubit/__brick__/{{name.lowerCase()}}/logic/{{name.lowerCase()}}_cubit/{{name.lowerCase()}}_cubit.dart
@@ -3,6 +3,6 @@ import 'package:equatable/equatable.dart';
 
 part '{{name.lowerCase()}}_state.dart';
 
-class {{name.pascalCase()}}Bloc extends Cubit<{{name.pascalCase()}}State> {
+class {{name.pascalCase()}}Cubit extends Cubit<{{name.pascalCase()}}State> {
   {{name.pascalCase()}}Cubit() : super({{name.pascalCase()}}Initial()) ;
 }
